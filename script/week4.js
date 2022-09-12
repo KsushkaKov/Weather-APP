e6fe04d6146bbc067dc7d6c461db76e7c15ebedb
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-09-12 09:59:05 +0300

    Changed current city actual css styles

--- script/week4.js
+++ script/week4.js
@@ -107,8 +107,6 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-// displayForecast();
-
 function searchLocation(position) {
   let apiKey = "6e9697292d87e7ccc444c909587e4383";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
